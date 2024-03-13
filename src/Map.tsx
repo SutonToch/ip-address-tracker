942c0c01e93f8bd0fad13a7e3ff98f0bd1954917
SutonToch <77286229+SutonToch@users.noreply.github.com>
2024-03-13 08:43:41 +0100

    FIX zoom control showing up on map

--- src/Map.tsx
+++ src/Map.tsx
@@ -9,11 +9,12 @@ export default function Map() {
     const longitude = -0.09;
   
     return (
-        <MapContainer center={[latitude, longitude]} zoom={13} ref={mapRef} style={{height: "66%", width: "100vw", marginTop: "-5px"}}>
+        <MapContainer center={[latitude, longitude]} zoom={13} ref={mapRef} zoomControl={false} style={{height: "67%", width: "100vw", marginTop: "-5px"}}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
+
         </MapContainer>
     )
 }
