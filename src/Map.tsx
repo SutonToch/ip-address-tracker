f0273c1a6e170cdfd708d4fe87f2e7cc9edff7ae
SutonToch <77286229+SutonToch@users.noreply.github.com>
2024-03-14 09:06:29 +0100

    ADD custom marker

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,6 +1,8 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import RecenterMap from "./RecenterMap.tsx";
+import L from "leaflet";
+import IconSVG from './assets/icon-location.svg'
 
 interface MapProps {
   latitude:number,
@@ -8,6 +10,12 @@ interface MapProps {
 }
 
 export default function Map(props:MapProps) {
+
+    const myIcon = L.icon({
+      iconUrl: IconSVG,
+      iconSize: [48,56],
+      iconAnchor: [32, 64],
+    })
   
     return (
         <MapContainer center={[props.latitude, props.longitude]} zoom={13}  zoomControl={false} style={{height: "67%", width: "100vw", marginTop: "-4px"}}>
@@ -15,6 +23,7 @@ export default function Map(props:MapProps) {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
+          <Marker position={[props.latitude, props.longitude]} icon={myIcon}></Marker>
           <RecenterMap 
             lat={props.latitude}
             lng={props.longitude}
